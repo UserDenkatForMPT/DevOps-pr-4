ebc3eb4645b12473b1d5d80d376a7bb8364d579f
Lebedev Vitaliy Sergeevich <isip_v.s.lebedev@mpt.ru>
2023-10-16 01:10:01 +0300

    first commit

---
+++ game-java/fr.bat
@@ -0,0 +1,45 @@
+@echo off
+
+:: Имя контейнера и образа
+set container_name=gamejavacont
+set image_name=gamejavaimg
+
+chcp 65001
+
+:: желтый
+echo [33m[INFO] Начинаю остановку контейнера %container_name%.[0m
+:: Остановить и удалить контейнер
+docker stop %container_name%
+docker rm %container_name%
+
+:: зеленый
+echo [32m[INFO] Старый контейнер %container_name% остановлен и удален.
+echo. 
+echo.
+:: color
+
+echo [33m[INFO] Начинаю остановку контейнеров, которые используют образ %image_name%.[0m
+:: Удалить образ и связанные с ним контейнеры
+for /f %%i in ('docker ps -a --filter "ancestor=%image_name%" -q') do (
+    docker stop %%i
+    docker rm %%i
+)
+echo [32m[INFO] Остановлены контейнеры, которые использовали образ %image_name%.[0m
+echo. 
+echo.
+
+echo [33m[INFO] Начинаю удаление старого образа %image_name%.[0m
+docker rmi %image_name%
+echo [32m[INFO] Старый образ %image_name% удален.[0m
+echo. 
+echo.
+
+echo [33m[INFO] Начинаю сборку образа %image_name%.[0m
+docker build -t %image_name% .
+echo [32m[INFO] Образ %image_name% собран.[0m
+echo. 
+echo.
+
+:: Запустить контейнер с обновленным образом
+docker run --name %container_name% -it %image_name% 
+@REM echo [INFO] container %container_name% run.
